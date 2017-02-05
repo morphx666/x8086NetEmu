cb0e681ba593d1572c0bcfdab5a69518d36a87d3
Xavier Flix <morphx@gmail.com>
2017-02-05 19:37:37 -0400

    Fixed cursor position

--- x8086NetEmu/My Project/AssemblyInfo.vb
+++ x8086NetEmu/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2017.2.5.1039")>
-<Assembly: AssemblyFileVersion("2017.2.5.1033")>
+<Assembly: AssemblyVersion("2017.2.5.1067")>
+<Assembly: AssemblyFileVersion("2017.2.5.1061")>
--- x8086NetEmuConsole/My Project/AssemblyInfo.vb
+++ x8086NetEmuConsole/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2017.2.5.144")>
-<Assembly: AssemblyFileVersion("2017.2.5.139")>
+<Assembly: AssemblyVersion("2017.2.5.147")>
+<Assembly: AssemblyFileVersion("2017.2.5.142")>
