e6ea7990a707f0dae3fe08763fb1fa1b69bbf2a2
Xavier Flix <morphx@gmail.com>
2019-02-28 21:23:57 -0400

    Updated to NAudio 1.8.5

--- x8086NetEmuConsole/My Project/AssemblyInfo.vb
+++ x8086NetEmuConsole/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2019.3.1.906")>
-<Assembly: AssemblyFileVersion("2019.3.1.901")>
+<Assembly: AssemblyVersion("2019.3.1.907")>
+<Assembly: AssemblyFileVersion("2019.3.1.902")>
