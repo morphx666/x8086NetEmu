74a2a1acad6f7f6a44756c0ac77626e2a60473d5
Xavier Flix <morphx@gmail.com>
2026-07-21 20:40:14 -0400

    Fix default BasePath

--- Eto.Forms/x8086NetEmuEto/MainForm.jeto.cs
+++ Eto.Forms/x8086NetEmuEto/MainForm.jeto.cs
@@ -12,7 +12,7 @@ namespace x8086NetEmuEto {
     public class MainForm : Form {
         private X8086 cpu;
         protected Drawable Canvas;
-        private readonly string basePath = @"..\..\";
+        private readonly string basePath = @"";
         private string runningApp = "";
 
         public MainForm() {
